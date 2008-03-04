ca1b9db9b6fd9b503a9c7304bad772b7cb1d1cf2
Dave Dribin <dave@dribin.org>
2008-03-03 23:21:14 -0600

    Add Rubyforge scripts and basic HTML page

---
+++ ruby/rubyforge/upload-api.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rsync -azv rdoc/ ddribin@rubyforge.org:/var/www/gforge-projects/lolspeak/api/
---
+++ ruby/rubyforge/upload-docroot.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rsync -azv rubyforge/docroot/ ddribin@rubyforge.org:/var/www/gforge-projects/lolspeak/
